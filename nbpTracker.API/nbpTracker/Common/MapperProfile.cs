d760bac6bd40d8056e64f7ab0d9a86054fdecd7a
konradkoziel <konrad981204@gmail.com>
2025-10-17 23:29:52 +0200

    Added DbContext and AutoMapper

---
+++ nbpTracker.API/nbpTracker/Common/MapperProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using nbpTracker.Model.Dto;
+using nbpTracker.Model.Entities;
+
+namespace nbpTracker.Common
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<ExchangeTable, ExchangeTableDto>();
+
+            CreateMap<CurrencyRate, CurrencyRateDto>()
+                .ForMember(dest => dest.CurrencyCode, opt => opt.MapFrom(src => src.Currency != null ? src.Currency.CurrencyCode : null))
+                .ForMember(dest => dest.CurrencyName, opt => opt.MapFrom(src => src.Currency != null ? src.Currency.CurrencyName : null));
+
+            CreateMap<Currency, CurrencyRateDto>()
+                .ForMember(dest => dest.CurrencyCode, opt => opt.MapFrom(src => src.CurrencyCode))
+                .ForMember(dest => dest.CurrencyName, opt => opt.MapFrom(src => src.CurrencyName))
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.Mid, opt => opt.Ignore());
+        }
+    }
+}
