40c81c8a57fc2d4b66ec20e447f07faefaaed3ea
konradkoziel <konrad981204@gmail.com>
2025-10-17 23:30:38 +0200

    Added ResultPAttern Result.cs

---
+++ nbpTracker.API/nbpTracker/Common/Result.cs
@@ -0,0 +1,14 @@
+﻿namespace nbpTracker.Common
+{
+    public record Result<T>(bool IsSuccess, T? Value, string? Error)
+    {
+        public static Result<T> Ok(T value) => new(true, value, null);
+        public static Result<T> Fail(string error) => new(false, default, error);
+    }
+    public record Result(bool IsSuccess, string? Error)
+    {
+        public static Result Ok() => new(true, null);
+        public static Result Fail(string error) => new(false, error);
+    }
+
+}
